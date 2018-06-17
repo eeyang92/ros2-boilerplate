308a35b14bf64335ad87baa767574b7b7dda26b9
Eric Yang <eeyang92@gmail.com>
2018-06-16 23:00:00 -0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# ROS2 Boilerplate
+
+## Setup
+
+- Install ROS2 for your platform, make sure you have sourced the setup files
+
+## To Build
+
+- `ament build --symlink-install --only-package boilerplate1`
---
+++ src/boilerplate1/setup.py
@@ -0,0 +1,31 @@
+'setup.py'
+
+from setuptools import setup
+
+setup(
+    name='boilerplate1',
+    version='0.0.1',
+    packages=[],
+    py_modules=[],
+    install_requires=['setuptools'],
+    author='Eric Yang',
+    author_email='eeyang92@gmail.com',
+    maintainer='Eric Yang',
+    maintainer_email='eeyang92@gmail.com',
+    keywords=['ROS', 'Boilerplate'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='ROS2 Boilerplate V1',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    entry_points={
+        'console_scripts': [
+            'listener_py = listener_py:main',
+            'talker_py = talker_py:main',
+        ],
+    },
+)
